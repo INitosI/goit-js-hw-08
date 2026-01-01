7e697b4e2e112e9f5cd38df211f92a7d2df0c8e6
Danil <150470763+INitosI@users.noreply.github.com>
2026-01-01 17:31:17 +0100

    Initial commit

---
+++ js/task-5.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
+
+const changeColorBtn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+	const bkgColor = getRandomHexColor();
+	document.body.style.backgroundColor = bkgColor;
+	colorName.textContent = bkgColor;
+});
